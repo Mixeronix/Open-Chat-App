391f258aedccf3c5d2aa54752367c08dd3790335
Bartosz Wiaderek <bartoszwiaderek@gmail.com>
2023-02-16 20:28:17 +0100

    Anonymous user

--- src/SignIn.js
+++ src/SignIn.js
@@ -16,6 +16,14 @@ export default function SignIn() {
 		});
 	};
 
+	const anonymous = async () => {
+		const response = await fetch("https://ipapi.co/json/");
+		const data = await response.json();
+
+		Cookies.set("user", data.ip);
+		window.location.reload(false);
+	};
+
 	return (
 		<div className="bg-gray w-full h-screen grid place-items-center">
 			<h1 className="text-5xl md:text-8xl text-gray-100 font-extrabold text-center ">Open Chat</h1>
@@ -25,6 +33,13 @@ export default function SignIn() {
 				onClick={signInWithGoogle}
 			>
 				Zaloguj się z Google
+			</button>
+
+			<button
+				className="sign-in bg-accent text-white px-10  md:px-16 py-7  sm:py-10 md:py-14 text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold rounded-lg"
+				onClick={anonymous}
+			>
+				Czatuj anonimowo
 			</button>
 		</div>
 	);
